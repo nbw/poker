ce3c054ce936d1b44d562edacdd3f51d3275c080
Nathan Willson <willson.b.nathan@gmail.com>
2020-05-24 12:52:05 +0900

    Heroku deploy config
    https://hexdocs.pm/phoenix/heroku.html

--- lib/poker_web/endpoint.ex
+++ lib/poker_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule PokerWeb.Endpoint do
   ]
 
   socket "/socket", PokerWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]

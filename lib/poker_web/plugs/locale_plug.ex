3f3b1f0d5339b9986a95dbf1362ebafcbce062aa
Nathan Willson <willson.b.nathan@gmail.com>
2020-06-05 22:07:14 +0900

    Store locale as cookie; language buttons on room page

--- lib/poker_web/plugs/locale_plug.ex
+++ lib/poker_web/plugs/locale_plug.ex
@@ -9,16 +9,60 @@ defmodule PokerWeb.Plugs.Locale do
   def call(%Plug.Conn{params: %{"locale" => locale}} = conn, _opts) when locale in @locales do
     Gettext.put_locale(PokerWeb.Gettext, locale)
 
-    put_session(conn, :locale, locale)
+    conn
+    |> put_resp_cookie("locale", locale, max_age: 10*24*60*60)
+    |> put_session(:locale, locale)
   end
 
   # Maintain locale
   def call(conn, _opts) do
-    case get_session(conn, :locale) do
-      nil -> conn
-        put_session(conn, :locale, Gettext.get_locale())
-      locale ->
-        put_session(conn, :locale, locale)
+    locale = case get_session(conn, :locale) ||
+      locale_from_cookies(conn) ||
+        locale_from_header(conn) do
+      nil -> put_session(conn, :locale, Gettext.get_locale())
+      loc -> loc
+    end
+
+    Gettext.put_locale(PokerWeb.Gettext, locale)
+
+    conn
+  end
+
+  defp locale_from_cookies(conn) do
+    conn.cookies["locale"]
+  end
+
+  defp locale_from_header(conn) do
+    conn
+    |> extract_accept_language
+    |> Enum.find(nil, fn accepted_locale -> Enum.member?(@locales, accepted_locale) end)
+  end
+  def extract_accept_language(conn) do
+    case Plug.Conn.get_req_header(conn, "accept-language") do
+      [value | _] ->
+        value
+        |> String.split(",")
+        |> Enum.map(&parse_language_option/1)
+        |> Enum.sort(&(&1.quality > &2.quality))
+        |> Enum.map(&(&1.tag))
+        |> Enum.reject(&is_nil/1)
+        |> ensure_language_fallbacks()
+      _ ->
+        []
+    end
+  end
+  defp parse_language_option(string) do
+    captures = Regex.named_captures(~r/^\s?(?<tag>[\w\-]+)(?:;q=(?<quality>[\d\.]+))?$/i, string)
+    quality = case Float.parse(captures["quality"] || "1.0") do
+      {val, _} -> val
+      _ -> 1.0
+    end
+    %{tag: captures["tag"], quality: quality}
+  end
+  defp ensure_language_fallbacks(tags) do
+    Enum.flat_map tags, fn tag ->
+      [language | _] = String.split(tag, "-")
+      if Enum.member?(tags, language), do: [tag], else: [tag, language]
     end
   end
 end

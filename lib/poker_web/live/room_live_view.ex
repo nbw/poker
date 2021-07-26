66a96293bda7a5b6d9d20e5ee0f0f0cb54b6b4cb
Nathan Willson <willson.b.nathan@gmail.com>
2021-07-26 12:24:23 +0900

    add heartbeat to keep socket alive (#24)

--- lib/poker_web/live/room_live_view.ex
+++ lib/poker_web/live/room_live_view.ex
@@ -3,46 +3,67 @@ defmodule PokerWeb.RoomLiveView do
 
   alias PokerWeb.Presence
 
-  @scores [1,2,3,4,5,8,10,20]
+  @scores [1, 2, 3, 4, 5, 8, 10, 20]
   def scores, do: @scores
 
   def render(assigns) do
     PokerWeb.RoomView.render("show.html", assigns)
   end
 
-  def mount(_, %{"room_id"=> room_id, "user_token"=> user_token, "locale" => locale}, socket) do
+  def mount(_, %{"room_id" => room_id, "user_token" => user_token, "locale" => locale}, socket) do
     # Subscribe to webhook to:
     # 1. send/broadcast messages
     # 2. receive presence_diff messages
     Phoenix.PubSub.subscribe(Poker.PubSub, topic(room_id))
     Phoenix.PubSub.subscribe(Poker.PubSub, topic_state(room_id))
 
+    if connected?(socket), do: Process.send_after(self(), :heartbeat, 1000)
+
     {:ok,
-      assign(socket,
-        state: get_game_state(room_id),
-        current_user: nil,
-        room_id: room_id,
-        user_name: "",
-        user_observer: false,
-        user_token: user_token,
-        users: users_list(room_id),
-        locale: locale
-      )}
+     assign(socket,
+       state: get_game_state(room_id),
+       current_user: nil,
+       room_id: room_id,
+       user_name: "",
+       user_observer: false,
+       user_token: user_token,
+       users: users_list(room_id),
+       locale: locale,
+       heartbeat: 0
+     )}
+  end
+
+  def handle_info(:heartbeat, %{assigns: %{heartbeat: h}} = socket) do
+    Process.send_after(self(), :heartbeat, 1000)
+
+    {:noreply, assign(socket, :heartbeat, rem(h + 1, 100))}
   end
 
   # Channels/Presence: handle "reset" message
-  def handle_info(%{event: "reset"}, %{assigns: %{room_id: room_id, user_observer: true}} = socket) do
+  def handle_info(
+        %{event: "reset"},
+        %{assigns: %{room_id: room_id, user_observer: true}} = socket
+      ) do
     update_game_state(create_game_state(room_id), room_id)
 
     {:noreply, socket}
   end
 
-  def handle_info(%{event: "reset"},
-    %{assigns: %{room_id: room_id, user_token: user_token, user_name: name, user_observer: obs}} = socket)
-  do
+  def handle_info(
+        %{event: "reset"},
+        %{
+          assigns: %{
+            room_id: room_id,
+            user_token: user_token,
+            user_name: name,
+            user_observer: obs
+          }
+        } = socket
+      ) do
     # Reset user to defaults
-    user = create_user(user_token, name, obs)
-           |> update_user(room_id, user_token)
+    user =
+      create_user(user_token, name, obs)
+      |> update_user(room_id, user_token)
 
     # Reset game state
     update_game_state(create_game_state(room_id), room_id)
@@ -52,9 +73,9 @@ defmodule PokerWeb.RoomLiveView do
 
   # Channels/Presence: When a change on a Presense object occurs
   def handle_info(
-    %{event: "presence_diff", payload: _},
-    %{assigns: %{room_id: room_id, user_token: user_token}} = socket)
-  do
+        %{event: "presence_diff", payload: _},
+        %{assigns: %{room_id: room_id, user_token: user_token}} = socket
+      ) do
     new_state = [
       users: users_list(room_id),
       state: get_game_state(room_id),
@@ -65,63 +86,67 @@ defmodule PokerWeb.RoomLiveView do
   end
 
   # LiveView: Handle card selection
-  def handle_event("card", _, %{assigns: %{current_user: %{observer: true}}} = socket), do: {:noreply, socket}
-  def handle_event("card", _, %{assigns: %{state: %{edit_lock: true, finish: true}}} = socket), do: {:noreply, socket}
-  def handle_event("card",
-    %{"num" => num},
-    %{assigns: %{room_id: room_id, user_token: user_token}} = socket)
-  do
-    {num, _} = Integer.parse(num) # Convert to integer
+  def handle_event("card", _, %{assigns: %{current_user: %{observer: true}}} = socket),
+    do: {:noreply, socket}
+
+  def handle_event("card", _, %{assigns: %{state: %{edit_lock: true, finish: true}}} = socket),
+    do: {:noreply, socket}
+
+  def handle_event(
+        "card",
+        %{"num" => num},
+        %{assigns: %{room_id: room_id, user_token: user_token}} = socket
+      ) do
+    # Convert to integer
+    {num, _} = Integer.parse(num)
 
     update_user(%{score: num}, room_id, user_token)
 
     if all_users_voted?(room_id) do
-      update_game_state(%{ finish: true }, room_id)
+      update_game_state(%{finish: true}, room_id)
     end
 
     {:noreply, socket}
   end
 
   # LiveView: Task title update
-  def handle_event("task_title", %{"title" => %{"query" => query}},
-    %{assigns: %{room_id: room_id}} = socket)
-  do
-    update_game_state(%{ task_title: query }, room_id)
+  def handle_event(
+        "task_title",
+        %{"title" => %{"query" => query}},
+        %{assigns: %{room_id: room_id}} = socket
+      ) do
+    update_game_state(%{task_title: query}, room_id)
 
     {:noreply, socket}
   end
 
   # LiveView: Task URL update
-  def handle_event("task_url", %{"url" => %{"query" => query}},
-    %{assigns: %{room_id: room_id}} = socket)
-  do
-    update_game_state(%{ task_url: query }, room_id)
+  def handle_event(
+        "task_url",
+        %{"url" => %{"query" => query}},
+        %{assigns: %{room_id: room_id}} = socket
+      ) do
+    update_game_state(%{task_url: query}, room_id)
 
     {:noreply, socket}
   end
 
   # LiveView: Start button
-  def handle_event("start", _,
-    %{assigns: %{room_id: room_id}} = socket)
-  do
-    update_game_state(%{ start: true }, room_id)
+  def handle_event("start", _, %{assigns: %{room_id: room_id}} = socket) do
+    update_game_state(%{start: true}, room_id)
 
     {:noreply, socket}
   end
 
   # LiveView: Finish button
-  def handle_event("finish", _,
-    %{assigns: %{room_id: room_id}} = socket)
-  do
-    update_game_state(%{ finish: true }, room_id)
+  def handle_event("finish", _, %{assigns: %{room_id: room_id}} = socket) do
+    update_game_state(%{finish: true}, room_id)
 
     {:noreply, socket}
   end
 
   # LiveView: Reset button
-  def handle_event("reset", _,
-    %{assigns: %{room_id: room_id}} = socket)
-  do
+  def handle_event("reset", _, %{assigns: %{room_id: room_id}} = socket) do
     # Broadcast a "reset" message to everyone via webhook.
     # 理由: can only update presence objects you're tracking,
     # which doesn't include other users.
@@ -130,15 +155,17 @@ defmodule PokerWeb.RoomLiveView do
     {:noreply, socket}
   end
 
-  def handle_event("edit_lock", _,
-    %{
-      assigns: %{
-        room_id: room_id,
-        state: %{ edit_lock: edit_lock }
-      }
-    } = socket)
-  do
-    update_game_state(%{ edit_lock: !edit_lock }, room_id)
+  def handle_event(
+        "edit_lock",
+        _,
+        %{
+          assigns: %{
+            room_id: room_id,
+            state: %{edit_lock: edit_lock}
+          }
+        } = socket
+      ) do
+    update_game_state(%{edit_lock: !edit_lock}, room_id)
 
     {:noreply, socket}
   end
@@ -151,9 +178,12 @@ defmodule PokerWeb.RoomLiveView do
   # LiveView: Join button
   # - Do nothing if name is blank
   def handle_event("create_user", %{"name" => ""}, socket), do: {:noreply, socket}
-  def handle_event("create_user", %{"obs" => obs},
-    %{assigns: %{room_id: room_id, user_token: user_token, user_name: name}} = socket)
-  do
+
+  def handle_event(
+        "create_user",
+        %{"obs" => obs},
+        %{assigns: %{room_id: room_id, user_token: user_token, user_name: name}} = socket
+      ) do
     user = create_user(user_token, name, observe(obs))
 
     Presence.track(
@@ -237,7 +267,9 @@ defmodule PokerWeb.RoomLiveView do
     Presence.list(topic)
     |> Map.fetch(room_id)
     |> case do
-      {:ok, s} -> s
+      {:ok, s} ->
+        s
+
       :error ->
         # Register the process so everyone can access it.
         # (this is so anyone can update the presence object)
